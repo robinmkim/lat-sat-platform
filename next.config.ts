ad8fd74554c8bbb9dce33d0535f7471a6e14c55c
robinmkim <ickhigh@gmail.com>
2025-06-04 23:35:06 +0900

    🐛 ㅇ

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,19 @@
 import type { NextConfig } from "next";
 
-// ✅ CommonJS 모듈은 require 방식으로 import
-
-const PrismaPlugin = require("@prisma/nextjs-monorepo-workaround-plugin");
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const applyPrismaPlugin = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 const nextConfig: NextConfig = {
   eslint: {
-    // eslint 에러가 있어도 빌드 진행
     ignoreDuringBuilds: true,
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
+      // ✅ 함수로 호출
+      applyPrismaPlugin(config);
     }
     return config;
   },
-  // ✅ Prisma 바이너리가 포함되도록 설정
   output: "standalone",
 };
 
