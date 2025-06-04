e07d214284d8bf25abf9eb968024b7848975d448
robinmkim <ickhigh@gmail.com>
2025-06-04 22:08:39 +0900

    🐛 플러그인 추가

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import PrismaPlugin from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -6,6 +7,12 @@ const nextConfig: NextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
   },
   optimizePackageImports: ["@prisma/client"],
   output: "standalone", // ✅ Prisma 바이너리 포함을 보장
